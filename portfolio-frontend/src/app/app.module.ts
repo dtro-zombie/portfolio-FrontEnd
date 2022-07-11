3ede513e6dab4dd084e62b4f32443abc75fbea85
nahuel arias <53919868+dtro-zombie@users.noreply.github.com>
2022-07-11 00:34:43 -0300

    banner y header mejorados

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -4,18 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialesComponent } from './components/sociales/sociales.component';
+
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent,
-    SocialesComponent,
+   
     BannerComponent,
+    AcercaDeComponent,
     
   ],
   imports: [
--- portfolio-frontend/src/app/components/acerca-de/acerca-de.component.spec.ts
+++ portfolio-frontend/src/app/components/acerca-de/acerca-de.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LogoAPComponent } from './logo-ap.component';
+import { AcercaDeComponent } from './acerca-de.component';
 
-describe('LogoAPComponent', () => {
-  let component: LogoAPComponent;
-  let fixture: ComponentFixture<LogoAPComponent>;
+describe('AcercaDeComponent', () => {
+  let component: AcercaDeComponent;
+  let fixture: ComponentFixture<AcercaDeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LogoAPComponent ]
+      declarations: [ AcercaDeComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(LogoAPComponent);
+    fixture = TestBed.createComponent(AcercaDeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
