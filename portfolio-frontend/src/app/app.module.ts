59000d112bf2c23b22d5fbb66935743b6b024327
nahuel arias <53919868+dtro-zombie@users.noreply.github.com>
2022-07-17 17:08:15 -0300

    edu y xp mejorados

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './components/header/header.component';
 
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 
 @NgModule({
@@ -16,6 +18,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
    
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
     
   ],
   imports: [
