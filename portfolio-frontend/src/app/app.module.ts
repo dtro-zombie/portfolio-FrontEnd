7dab43ab74b319323b0537e54d88bc6863bfe02b
nahuel arias <53919868+dtro-zombie@users.noreply.github.com>
2022-08-02 21:59:56 -0300

    implementando login

--- portfolio-frontend/src/app/app.module.ts
+++ portfolio-frontend/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
  @NgModule({
   declarations: [
     AppComponent,
@@ -37,7 +38,7 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
